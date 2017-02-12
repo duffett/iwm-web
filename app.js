f0404d589ba33d0bd18315daeb5eca55ecb7ba4f
Christian Duffett <duffett@gmail.com>
2017-02-12 14:25:02 -0500

    ...

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ var path = require('path');
 var serveStatic = require('serve-static')
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port);
 console.log('server started on port ' + port);
