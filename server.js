adf2bc2f22e674647dbc7209fd00833b27fcbf7a
Christian Duffett <duffett@gmail.com>
2017-02-12 11:49:33 -0500

    run production server locallly

---
+++ server.js
@@ -0,0 +1,11 @@
+// from
+// https: //medium.com/@sagarjauhari/quick-n-dirty-way-to-deploying-vue-webpack-apps-on-heroku-4ab964ee536#.60la0ef0k
+
+var express = require('express')
+var path = require('path');
+var serveStatic = require('serve-static')
+app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')));
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started on port ' + port);
