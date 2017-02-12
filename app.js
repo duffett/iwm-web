72647635c352200e562609e2c8de3bf3552e3de5
Christian Duffett <duffett@gmail.com>
2017-02-12 14:47:30 -0500

    ...

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
 // from
 // https: //medium.com/@sagarjauhari/quick-n-dirty-way-to-deploying-vue-webpack-apps-on-heroku-4ab964ee536#.60la0ef0k
 
-var express = require('express')
+var express = require('express');
 var path = require('path');
-var serveStatic = require('serve-static')
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+// var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
+
+var app = express();
+
+// app.use(logger('dev'));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// app.use(serveStatic(path.join(__dirname, 'dist')));
 // var port = process.env.PORT || 3000;
 // app.listen(port);
 // console.log('server started on port ' + port);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 
 module.exports = app;
